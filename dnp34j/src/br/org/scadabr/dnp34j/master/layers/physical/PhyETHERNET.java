6b1f5c3a6f5a7eaf4ae1c114f82df40aa6a5833f
terrypacker <terrypacker@users.noreply.github.com>
2020-05-08 11:07:20 -1000

    Don't blindly munch exceptions on init

--- dnp34j/src/br/org/scadabr/dnp34j/master/layers/physical/PhyETHERNET.java
+++ dnp34j/src/br/org/scadabr/dnp34j/master/layers/physical/PhyETHERNET.java
@@ -5,225 +5,211 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 import br.org.scadabr.dnp34j.master.common.InitFeatures;
 
 /**
  * DOCUMENT ME!
- * 
+ *
  * @author $author$
  * @version $Revision: 1.1.1.1 $
  */
 public class PhyETHERNET implements InitFeatures {
-	static final boolean DEBUG = !LNK_QUIET;
+    static final boolean DEBUG = !LNK_QUIET;
 
-	// =============================================================================
-	// Attributes
-	// =============================================================================
-	private InputStream inputStream;
-	private OutputStream outputStream;
-	// private String commAddress;
-	private String host;
-	private int port;
-	private PhyLayer phyLayer;
-	private Socket socket;
-	private ServerSocket serverSocket;
+    // =============================================================================
+    // Attributes
+    // =============================================================================
+    private InputStream inputStream;
+    private OutputStream outputStream;
+    // private String commAddress;
+    private String host;
+    private int port;
+    private PhyLayer phyLayer;
+    private Socket socket;
+    private ServerSocket serverSocket;
 
-	// =============================================================================
-	// Constructor
-	// =============================================================================
-	public PhyETHERNET() {
-	}
+    // =============================================================================
+    // Constructor
+    // =============================================================================
+    public PhyETHERNET() {
+    }
 
-	public PhyETHERNET(PhyLayer parent) throws Exception {
-		this.phyLayer = parent;
-		initialize(phyLayer);
-	}
+    public PhyETHERNET(PhyLayer parent) throws Exception {
+        this.phyLayer = parent;
+        initialize(phyLayer);
+    }
 
-	// =============================================================================
-	// Methods
-	// =============================================================================
+    // =============================================================================
+    // Methods
+    // =============================================================================
 
-	public void init() throws Exception {
-		initialize(phyLayer);
-	}
+    public void init() throws Exception {
+        initialize(phyLayer);
+    }
 
-	/**
-	 * DOCUMENT ME!
-	 * 
-	 * @throws Exception
-	 */
-	private void initialize(PhyLayer phyLayer) throws Exception {
+    /**
+     * DOCUMENT ME!
+     *
+     * @throws Exception
+     */
+    private void initialize(PhyLayer phyLayer) throws Exception {
 
-		host = phyLayer.getCommAddress();
-		port = phyLayer.getPort();
+        host = phyLayer.getCommAddress();
+        port = phyLayer.getPort();
 
-		boolean connectionAccepted = false;
+        boolean connectionAccepted = false;
 
-		if (DEBUG) {
-			System.out.println("Trying to connect to:");
-			System.out.println("http://" + host + ":" + port);
-		}
+        if (DEBUG) {
+            System.out.println("Trying to connect to:");
+            System.out.println("http://" + host + ":" + port);
+        }
 
-		try {
-			socket = new Socket(host, port);
+        socket = new Socket(host, port);
 
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        if (DEBUG) {
+            System.out.println("Created socket:");
+            System.out.println(socket);
+        }
+        while (!connectionAccepted) {
+            connectionAccepted = true;
 
-		if (DEBUG) {
-			System.out.println("Created socket:");
-			System.out.println(socket);
-		}
-		while (!connectionAccepted) {
-			connectionAccepted = true;
+            if (DEBUG) {
+                System.out.println("[PhyLayer] Attempting to connect "
+                        + phyLayer.getUri());
+            }
+        }
 
-			if (DEBUG) {
-				System.out.println("[PhyLayer] Attempting to connect "
-						+ phyLayer.getUri());
-			}
-		}
+        if (DEBUG) {
+            System.out.println("[PhyLayer] Connected to " + phyLayer.getUri());
+        }
 
-		if (DEBUG) {
-			System.out.println("[PhyLayer] Connected to " + phyLayer.getUri());
-		}
+        setOutputStream(socket.getOutputStream());
+        setInputStream(socket.getInputStream());
+    }
 
-		try {
-			setOutputStream(socket.getOutputStream());
-			setInputStream(socket.getInputStream());
+    public void close() throws Exception {
+        closeClient();
+    }
 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    /**
+     * DOCUMENT ME!
+     *
+     * @throws Exception
+     */
+    protected void closeClient() throws Exception {
+        try {
+            if (socket != null)
+                socket.close();
+        } catch (IOException e) {
+            String msg = "[PhyETHERNET] - socket.close() failed. Exception throwed!";
+            e.printStackTrace();
+            throw new Exception(msg, e);
+        }
+    }
 
-	public void close() throws Exception {
-		closeClient();
-	}
+    /**
+     * @return the inputStream
+     */
+    public InputStream getInputStream() {
+        return inputStream;
+    }
 
-	/**
-	 * DOCUMENT ME!
-	 * 
-	 * @throws Exception
-	 */
-	protected void closeClient() throws Exception {
-		try {
-			if (socket != null)
-				socket.close();
-		} catch (IOException e) {
-			String msg = "[PhyETHERNET] - socket.close() failed. Exception throwed!";
-			e.printStackTrace();
-			throw new Exception(msg, e);
-		}
-	}
+    /**
+     * @param inputStream
+     *            the inputStream to set
+     */
+    public void setInputStream(InputStream inputStream) {
+        this.inputStream = inputStream;
+    }
 
-	/**
-	 * @return the inputStream
-	 */
-	public InputStream getInputStream() {
-		return inputStream;
-	}
+    /**
+     * @return the outputStream
+     */
+    public OutputStream getOutputStream() {
+        return outputStream;
+    }
 
-	/**
-	 * @param inputStream
-	 *            the inputStream to set
-	 */
-	public void setInputStream(InputStream inputStream) {
-		this.inputStream = inputStream;
-	}
+    /**
+     * @param outputStream
+     *            the outputStream to set
+     */
+    public void setOutputStream(OutputStream outputStream) {
+        this.outputStream = outputStream;
+    }
 
-	/**
-	 * @return the outputStream
-	 */
-	public OutputStream getOutputStream() {
-		return outputStream;
-	}
+    /**
+     * @return the port
+     */
+    public int getPort() {
+        return port;
+    }
 
-	/**
-	 * @param outputStream
-	 *            the outputStream to set
-	 */
-	public void setOutputStream(OutputStream outputStream) {
-		this.outputStream = outputStream;
-	}
+    /**
+     * @return the host
+     */
+    public String getHost() {
+        return host;
+    }
 
-	/**
-	 * @return the port
-	 */
-	public int getPort() {
-		return port;
-	}
+    /**
+     * @param host
+     *            the host to set
+     */
+    public void setHost(String host) {
+        this.host = host;
+    }
 
-	/**
-	 * @return the host
-	 */
-	public String getHost() {
-		return host;
-	}
+    /**
+     * @param port
+     *            the port to set
+     */
+    public void setPort(int port) {
+        this.port = port;
+    }
 
-	/**
-	 * @param host
-	 *            the host to set
-	 */
-	public void setHost(String host) {
-		this.host = host;
-	}
+    /**
+     * @return the phyLayer
+     */
+    public PhyLayer getPhyLayer() {
+        return phyLayer;
+    }
 
-	/**
-	 * @param port
-	 *            the port to set
-	 */
-	public void setPort(int port) {
-		this.port = port;
-	}
+    /**
+     * @param phyLayer
+     *            the phyLayer to set
+     */
+    public void setPhyLayer(PhyLayer phyLayer) {
+        this.phyLayer = phyLayer;
+    }
 
-	/**
-	 * @return the phyLayer
-	 */
-	public PhyLayer getPhyLayer() {
-		return phyLayer;
-	}
+    /**
+     * @return the socket
+     */
+    public Socket getSocket() {
+        return socket;
+    }
 
-	/**
-	 * @param phyLayer
-	 *            the phyLayer to set
-	 */
-	public void setPhyLayer(PhyLayer phyLayer) {
-		this.phyLayer = phyLayer;
-	}
+    /**
+     * @param socket
+     *            the socket to set
+     */
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
 
-	/**
-	 * @return the socket
-	 */
-	public Socket getSocket() {
-		return socket;
-	}
+    /**
+     * @return the serverSocket
+     */
+    public ServerSocket getServerSocket() {
+        return serverSocket;
+    }
 
-	/**
-	 * @param socket
-	 *            the socket to set
-	 */
-	public void setSocket(Socket socket) {
-		this.socket = socket;
-	}
-
-	/**
-	 * @return the serverSocket
-	 */
-	public ServerSocket getServerSocket() {
-		return serverSocket;
-	}
-
-	/**
-	 * @param serverSocket
-	 *            the serverSocket to set
-	 */
-	public void setServerSocket(ServerSocket serverSocket) {
-		this.serverSocket = serverSocket;
-	}
+    /**
+     * @param serverSocket
+     *            the serverSocket to set
+     */
+    public void setServerSocket(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
 }
